7404ed570fe65a9290cf74e409fb89447898e5d6
DavidKratky <51231236+DavidKratky@users.noreply.github.com>
2021-03-29 20:16:13 +0200

    Som, alebo niesom
    to je otázka

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php session_start() ?>
 <?php 
 	if(!isset($_SESSION['login_id']))
-	    header('location:login.php'); //som tu
+	    header('location:login.php'); //som tu hádam
     include 'db_connect.php';
     ob_start();
   if(!isset($_SESSION['system'])){
