504d1000223d52c1e9f9e1d4ca28af44a10b6e1b
AdamKocian <81522331+AdamKocian@users.noreply.github.com>
2021-03-29 20:21:34 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <?php session_start() ?>
 <?php 
 	if(!isset($_SESSION['login_id']))
-	    header('location:login.php'); //som tu hádam
+	    header('location:login.php'); //som tu hádam 2
     include 'db_connect.php';
     ob_start();
   if(!isset($_SESSION['system'])){
