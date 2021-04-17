b0b2f09458dbd416dab579600d32795cd72f28f0
AdamKocian <81522331+AdamKocian@users.noreply.github.com>
2021-04-17 13:17:33 +0200

    Harmonogram + záloha

--- harmonogram.php
+++ harmonogram.php
@@ -2,22 +2,22 @@
 <?php
 $twhere = "";
 if ($_SESSION['login_type'] != 1)
-  $twhere = "  ";
+    $twhere = "  ";
 ?>
 
 <?php
 
 $where = "";
 if ($_SESSION['login_type'] == 2) {
-  $where = " where manager_id = '{$_SESSION['login_id']}' ";
+    $where = " where manager_id = '{$_SESSION['login_id']}' ";
 } elseif ($_SESSION['login_type'] == 3) {
-  $where = " where concat('[',REPLACE(user_ids,',','],['),']') LIKE '%[{$_SESSION['login_id']}]%' ";
+    $where = " where concat('[',REPLACE(user_ids,',','],['),']') LIKE '%[{$_SESSION['login_id']}]%' ";
 }
 $where2 = "";
 if ($_SESSION['login_type'] == 2) {
-  $where2 = " where p.manager_id = '{$_SESSION['login_id']}' ";
+    $where2 = " where p.manager_id = '{$_SESSION['login_id']}' ";
 } elseif ($_SESSION['login_type'] == 3) {
-  $where2 = " where concat('[',REPLACE(p.user_ids,',','],['),']') LIKE '%[{$_SESSION['login_id']}]%' ";
+    $where2 = " where concat('[',REPLACE(p.user_ids,',','],['),']') LIKE '%[{$_SESSION['login_id']}]%' ";
 }
 ?>
 
@@ -25,147 +25,94 @@ if ($_SESSION['login_type'] == 2) {
 <html>
 
 <head>
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.css" />
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.0.0-alpha.6/css/bootstrap.css" />
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.18.1/moment.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/fullcalendar/3.4.0/fullcalendar.min.js"></script>
+    <meta charset='utf-8' />
+    <title>
+        Harmonogram
+    </title>
+    <style>
+        #calendar {
+            max-width: 100%;
+            margin: 10px auto;
+        }
+    </style>
+    <link href='https://unpkg.com/@fullcalendar/core@4.4.1/main.min.css' rel='stylesheet' />
+    <link href='https://unpkg.com/@fullcalendar/daygrid@4.4.1/main.min.css' rel='stylesheet' />
+    <link href='https://unpkg.com/@fullcalendar/timegrid@4.4.1/main.min.css' rel='stylesheet' />
+    <script src='https://unpkg.com/@fullcalendar/core@4.4.1/main.min.js'></script>
+    <script src='https://unpkg.com/@fullcalendar/interaction@4.4.0/main.min.js'></script>
+    <script src='https://unpkg.com/@fullcalendar/daygrid@4.4.1/main.min.js'></script>
+    <script src='https://unpkg.com/@fullcalendar/timegrid@4.4.1/main.min.js'></script>
+
     <script>
-        $(document).ready(function() {
-            var calendar = $('#calendar').fullCalendar({
-                editable: true,
+        document.addEventListener('DOMContentLoaded', function() {
+            var calendarEl = document.getElementById('calendar');
+
+            var calendar = new FullCalendar.Calendar(calendarEl, {
+                plugins: ['interaction', 'dayGrid', 'timeGrid'],
+                defaultView: 'dayGridMonth',
+                defaultDate: '2020-05-07',
                 header: {
                     left: 'prev,next today',
                     center: 'title',
-                    right: 'month,agendaWeek,agendaDay'
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
                 },
-                events: 'load.php',
-                selectable: true,
-                selectHelper: true,
-                select: function(start, end, allDay) {
-                    var title = prompt("Enter Event Title");
-                    if (title) {
-                        var start = $.fullCalendar.formatDate(start, "Y-MM-DD HH:mm:ss");
-                        var end = $.fullCalendar.formatDate(end, "Y-MM-DD HH:mm:ss");
-                        $.ajax({
-                            url: "insert.php",
-                            type: "POST",
-                            data: {
-                                title: title,
-                                start: start,
-                                end: end
-                            },
-                            success: function() {
-                                calendar.fullCalendar('refetchEvents');
-                                alert("Added Successfully");
-                            }
-                        })
+                events: [{
+                        title: 'All Day Event',
+                        start: '2020-05-01'
+                    },
+                    {
+                        title: 'Long Event',
+                        start: '2020-05-07',
+                        end: '2020-05-10'
+                    },
+                    {
+                        groupId: '999',
+                        title: 'Repeating Event',
+                        start: '2020-05-09T16:00:00'
+                    },
+                    {
+                        groupId: '999',
+                        title: 'Repeating Event',
+                        start: '2020-05-16T16:00:00'
+                    },
+                    {
+                        title: 'Conference',
+                        start: '2020-05-11',
+                        end: '2020-05-13'
+                    },
+                    {
+                        title: 'Meeting',
+                        start: '2020-05-12T10:30:00',
+                        end: '2020-05-12T12:30:00'
+                    },
+                    {
+                        title: 'Lunch',
+                        start: '2020-05-12T12:00:00'
+                    },
+                    {
+                        title: 'Meeting',
+                        start: '2020-05-12T14:30:00'
+                    },
+                    {
+                        title: 'Birthday Party',
+                        start: '2020-05-13T07:00:00'
+                    },
+                    {
+                        title: 'Click for Google',
+                        url: 'http://google.com/',
+                        start: '2020-05-28'
                     }
-                },
-                editable: true,
-                eventResize: function(event) {
-                    var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-                    var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-                    var title = event.title;
-                    var id = event.id;
-                    $.ajax({
-                        url: "update.php",
-                        type: "POST",
-                        data: {
-                            title: title,
-                            start: start,
-                            end: end,
-                            id: id
-                        },
-                        success: function() {
-                            calendar.fullCalendar('refetchEvents');
-                            alert('Event Update');
-                        }
-                    })
-                },
-
-                eventDrop: function(event) {
-                    var start = $.fullCalendar.formatDate(event.start, "Y-MM-DD HH:mm:ss");
-                    var end = $.fullCalendar.formatDate(event.end, "Y-MM-DD HH:mm:ss");
-                    var title = event.title;
-                    var id = event.id;
-                    $.ajax({
-                        url: "update.php",
-                        type: "POST",
-                        data: {
-                            title: title,
-                            start: start,
-                            end: end,
-                            id: id
-                        },
-                        success: function() {
-                            calendar.fullCalendar('refetchEvents');
-                            alert("Event Updated");
-                        }
-                    });
-                },
-
-                eventClick: function(event) {
-                    if (confirm("Are you sure you want to remove it?")) {
-                        var id = event.id;
-                        $.ajax({
-                            url: "delete.php",
-                            type: "POST",
-                            data: {
-                                id: id
-                            },
-                            success: function() {
-                                calendar.fullCalendar('refetchEvents');
-                                alert("Event Removed");
-                            }
-                        })
-                    }
-                },
-
+                ]
             });
+
+            calendar.render();
         });
     </script>
+
 </head>
 
 <body>
-        <div class="container">
-        <div id="calendar"></div>
-    </div>
+    <div id='calendar'></div>
 </body>
 
 </html>
-
-
-load.php
-
-
-<?php
-
-//load.php
-
-
-$connect = new mysqli('localhost', 'root', '', 'tms_db') or die("Could not connect to mysql" . mysqli_error($con));
-
-$data = array();
-
-$query = "SELECT * FROM events ORDER BY id";
-
-$statement = $connect->prepare($query);
-
-$statement->execute();
-
-$result = $statement->fetchAll();
-
-foreach ($result as $row) {
-    $data[] = array(
-        'id'   => $row["id"],
-        'title'   => $row["title"],
-        'start'   => $row["start_event"],
-        'end'   => $row["end_event"]
-    );
-}
-
-echo json_encode($data);
-
-?>
